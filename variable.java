f35424de4462de88b9c2b6f47532ad8fe198d49a
sha112419 <104012644+sha112419@users.noreply.github.com>
2022-05-12 14:05:26 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class variable {
+public void A()
+{
+int a=40;
+System.out.println(a);
+}
+
+public static void main(String[] args)
+{
+	int b=50;
+	System.out.println(b);
+	variable s=new variable();
+	s.A();
+	
+	
+}
+}
